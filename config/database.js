281c1cfb8eacfee5d7b3274c51a4f569af807fa4
Thomas Strub <Thom@Thomass-Air.wavecable.com>
2020-11-20 11:15:29 -0800

    user model added

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(// Enter your DB string, {
+mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true
@@ -10,5 +10,5 @@ const db = mongoose.connection;
 
 // database connection event
 db.on('connected', function () {
-  console.log(`Mongoose connected to: ${db.host}:${db.port}`);
+  console.log(`Mongoose connected to: Atlas}`);
 });
--- models/user.js
+++ models/user.js
@@ -1,3 +1,13 @@
 const mongoose = require('mongoose');
 
 // Create your User Model
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    avatar: String,
+    googleId: String
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('User', userSchema);
